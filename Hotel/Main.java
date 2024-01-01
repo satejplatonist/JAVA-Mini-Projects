0d825d5b2fbcf5d7af4e63d6180fe0ccbb2360a3
SATEJ DHAKANE <113412705+satejplatonist@users.noreply.github.com>
2024-01-01 11:56:59 +0530

    Update Main.java

--- Hotel/Main.java
+++ Hotel/Main.java
@@ -1,19 +1,12 @@
 package java_project_2;
 
-import java.util.Scanner;
-import java.lang.NullPointerException;
 
 public class Main
 {
 	public static void main(String args[])
 	{ 
-      Drink drink = new Drink();
-      drink.printDrinkPrice();
-      System.out.println(drink.totalPriceReturner());
-      
-      SideItem sideItem = new SideItem();
-      sideItem.printSideItemPrice();
-      System.out.println(sideItem.totalPriceReturner() + sideItem.getNameOfItem());
+      MealOrder mealOrder = new MealOrder();
+      mealOrder.MENU();
 	}
 }
 
